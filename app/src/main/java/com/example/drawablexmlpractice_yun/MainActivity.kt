b51d7b882124676e7a06432fa768e0f21abac21b
Roy <J.jyun0525@gmail.com>
2022-03-08 16:19:47 +0900

    양 옆의 여백공간 제거 toolbar설정에서 as Toolbar를 androidx로 선택! setContentInsetsAbsolute가 양쪽 여백

--- app/src/main/java/com/example/drawablexmlpractice_yun/MainActivity.kt
+++ app/src/main/java/com/example/drawablexmlpractice_yun/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.drawablexmlpractice_yun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
@@ -19,5 +20,13 @@ class MainActivity : AppCompatActivity() {
 //        어떤 커스텀 뷰를 사용할 껀지?
 
         defaultActionBar.setCustomView(R.layout.my_custom_actionbar)
+
+//        양 옆의 여백제거 ==> 커스텀 액션바를 들고있는 툴바를 찾아내서 여백 제거
+//        커스텀 뷰를 들고있는 툴바를 변수에 저장
+
+        val toolbar = defaultActionBar.customView.parent as androidx.appcompat.widget.Toolbar
+        toolbar.setContentInsetsAbsolute(0,0)
+
+
     }
 }
